9141527019fd3f696953b8451a3e2600db7a5aff
Michal Kulinski <you@example.com>
2020-05-18 07:55:11 +0200

    Small refactoring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 import connection.ServerConnection;
 
-public class Crypto {
+public class App {
 
     public static void main(String[] args) throws Exception {
 
--- src/main/java/utils/ApiConfig.java
+++ src/main/java/utils/ApiConfig.java
@@ -2,7 +2,7 @@ package utils;
 
 public class ApiConfig {
 
-    public static final String API_KEY = "35e522f2-3735-4986-8baa-6292fb32390a";
+    public static final String API_KEY = "<PUT_HERE_YOUR_API_KEY";
     public static final String API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
 
 }
